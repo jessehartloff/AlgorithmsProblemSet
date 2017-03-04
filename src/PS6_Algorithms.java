1dafc54ffe5e362fe72feb427b91e238a01a9053
jessehartloff <hartloff@buffalo.edu>
2017-03-04 15:22:39 -0500

    initial commit

---
+++ src/PS6_Algorithms.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
+
+public class PS6_Algorithms {
+
+
+
+    static int Q1(ArrayList<Integer> input){
+        // return the minimum value in the input ArrayList
+
+        return 0;
+    }
+
+
+    static int Q2(ArrayList<Integer> input){
+        // return the maximum value in the input ArrayList
+
+        return 0;
+    }
+
+
+    static int Q3(ArrayList<Integer> input){
+        // return the index containing the maximum value in the input ArrayList
+
+        return 0;
+    }
+
+
+    static double Q4(ArrayList<Integer> input){
+        // return the average of all the values in the input ArrayList
+
+        return 0.0;
+    }
+
+
+    static String Q5(HashMap<String, Integer> input){
+        // return the key corresponding to the maximum value in the input HashMap
+
+        return "";
+    }
+
+
+    static double Q6(ArrayList<Double> input){
+        // return the range (max - min) of the values in the input ArrayList
+
+        return 0.0;
+    }
+
+
+    static double Q7(ArrayList<Double> input){
+        // Return the rate of change of the values of the input ArrayList. The rate of change is computed with the
+        // first and last element of the list using the following formula: (last - first)/first
+        //
+        // Note: This is similar to computing ROI in hw2
+
+        return 0.0;
+    }
+
+
+    static int Q8(ArrayList<Integer> input){
+        // Return the 2nd highest value in the input ArrayList
+
+        return 0;
+    }
+
+
+    static int Q9(ArrayList<ArrayList<Integer>> input){
+        // Return the minimum value in the input list of lists (matrix)
+
+        return 0;
+    }
+
+
+    static String Q10(ArrayList<String> input){
+        // return the longest String in the input ArrayList. Ties can be broken arbitrarily
+
+        return "";
+    }
+
+
+    static double Q11(ArrayList<Integer> input1, ArrayList<Integer> input2){
+        // return the average of all the values in both input ArrayLists
+
+        return 0.0;
+    }
+
+
+    static double Q12(HashMap<String, ArrayList<Double>> input){
+        // return the maximum value contained in any of the ArrayLists in the input HashMap.
+
+
+        return 0.0;
+    }
+
+
+    static void Q13(ArrayList<Integer> input){
+        // sort the input ArrayList in increasing order
+
+    }
+
+
+    static double Q14(ArrayList<Integer> input){
+        // return the median value of the input ArrayList. If the size is even, return the average of the two
+        // elements closests to the median
+
+        return 0.0;
+    }
+
+
+    static int Q15(ArrayList<Integer> input){
+        // return the mode value of the input ArrayList. Ties can be broken arbitrarily
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
